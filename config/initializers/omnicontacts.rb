5623df5bf342e9cf51ce38b2a865b3aa4d91dea0
Khanh <nguyenvietnamkhanh@gmail.com>
2012-07-21 18:08:28 +0700

    reconfig omnicontacts

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,6 +1,7 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "189037423707-ufiquduq37s8qne5gjrceqo0hm27v5tg.apps.googleusercontent.com", "5aQqpiqFaq-Swx1nYPWl5h57", {:redirect_path => "/contacts/gmail/callback"}
-  importer :yahoo, "dj0yJmk9bXNBN3ltQkN5OEJNJmQ9WVdrOVJWazVNRWgzTmpRbWNHbzlPVEV5TURNeU9EWXkmcz1jb25zdW1lcnNlY3JldCZ4PTE2", "9edb80c1187522e4ad58a8fc724e2cdc4fb61ee4", {:callback_path => '/contacts/yahoo/callback'}  
+  importer :gmail, "client_id", "client_secret", {:redirect_path => "/contacts/gmail/callback"}
+  importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => '/contacts/yahoo/callback'}
+  #importer :hotmail, "client_id", "client_secret"
 end
