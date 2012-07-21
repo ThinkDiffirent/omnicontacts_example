4edc5fb8f5e0c37f57e0480c1d8bd23914d2e9ad
Khanh <nguyenvietnamkhanh@gmail.com>
2012-07-21 17:37:17 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmnicontactsExample::Application.config.secret_token = 'b10879853620b9204d477e156ac9f30f8ce8baade811fdb326bfe52a6b73b985925b2994de2eeeca570f1ec5438ec67af241e011d8fcae97df0d0aa5e0f82771'
